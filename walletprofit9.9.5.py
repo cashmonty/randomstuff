f5959486a0fd7d2f461cc8bd76b0b590f1c1e469
cashmonty <105658823+cashmonty@users.noreply.github.com>
2023-09-09 23:14:51 -0400

    Add files via upload
    successfully flipped the api call to make it much more efficient > first locate tx hash of transfers of erc20 tokens and then pull verbose transaction hash data on those transactions to then find and save the values for my wallet profit calculation

--- walletprofit9.9.5.py
+++ walletprofit9.9.5.py
@@ -11,15 +11,15 @@ load_dotenv(dotenv_path='moralisapi.env')
 api_key = os.getenv("API_KEY")
 chain = "eth"
 
-# Function to fetch transactions from Moralis
-def get_wallet_transactions(wallet_address):
-    params = {"chain": chain, "address": wallet_address}
-    return evm_api.transaction.get_wallet_transactions_verbose(api_key=api_key, params=params)
-
 # Function to fetch token transfers by block range
 def get_token_transfers(address):
     params = {"chain": chain, "address": address}
     return evm_api.token.get_wallet_token_transfers(api_key=api_key, params=params)
+
+# Function to fetch transactions from Moralis
+def transfer_tx_hash(transaction_hash):
+    params = {"chain": chain, "transaction_hash": transaction_hash}
+    return evm_api.transaction.get_transaction_verbose(api_key=api_key, params=params)
 
 # Read wallet addresses from CSV
 wallet_addresses = []
@@ -37,52 +37,46 @@ with open('ProfitabilityReport.csv', 'w', newline='') as csvfile:
     # Loop through each wallet address
     for wallet_address in wallet_addresses:
         transactions = {}
-        swap_tx_hashes = []
         total_trades = 0
         profitable_trades = 0
 
+        # Fetch token transfers for the wallet
+        token_transfers = get_token_transfers(wallet_address)
+
+        # Match transaction hashes to token addresses
+        for transfer in token_transfers.get('result', []):
+            transaction_hash = transfer.get('transaction_hash')
+            token_address = transfer.get('address')
+            transactions[transaction_hash] = {'token_address': token_address, 'spent_weth': 0, 'received_tokens': 0, 'sold_tokens': 0, 'received_weth': 0}
+    
+
         # Fetch transactions for the wallet
-        result = get_wallet_transactions(wallet_address)
+        for transaction_hash in transactions.keys():
+            result = transfer_tx_hash(transaction_hash)
 
-        # Loop through transactions to find 'Swap' events
-        for tx in result.get('result', []):
-            tx_hash = tx.get('hash')
-            block = tx.get('block_number')
+            # Loop through transactions to find 'Swap' events
+            for tx in result.get('result', []):
+                for log in tx.get('logs', []):
+                    transaction_value = int(log.get('transaction_value', '0'))
+                    is_buy = transaction_value > 0  # Determine if it's a buy or sell
+                    transactions[transaction_hash]['is_buy'] = is_buy
 
-            for log in tx.get('logs', []):
-                transaction_value = int(log.get('transaction_value', '0'))
+                    decoded_event = log.get('decoded_event', {})
+                    if decoded_event and decoded_event.get('signature') == 'Swap(address,uint256,uint256,uint256,uint256,address)':
+                        params = decoded_event.get('params', [])
+                        if len(params) >= 5:
+                            amount0In = int(params[1]['value'])
+                            amount1In = int(params[2]['value'])
+                            amount0Out = int(params[3]['value'])
+                            amount1Out = int(params[4]['value'])
 
-                decoded_event = log.get('decoded_event', {})
-                if decoded_event and decoded_event.get('signature') == 'Swap(address,uint256,uint256,uint256,uint256,address)':
-                    swap_tx_hashes.append(tx_hash)
-                    transactions.setdefault(tx_hash, {'is_buy': transaction_value > 0, 'block': block, 'spent_weth': 0, 'received_tokens': 0, 'sold_tokens': 0, 'received_weth': 0})
-
-                    params = decoded_event.get('params', [])
-                    
-                    if len(params) >= 5:
-                        amount0In = int(params[1]['value'])
-                        amount1In = int(params[2]['value'])
-                        amount0Out = int(params[3]['value'])
-                        amount1Out = int(params[4]['value'])
-
-                        if transaction_value > 0:  # This is a buy
-                            transactions[tx_hash]['spent_weth'] += amount1In
-                            transactions[tx_hash]['received_tokens'] += amount0Out
-                        else:  # This is a sell
-                            transactions[tx_hash]['sold_tokens'] += amount0In
-                            transactions[tx_hash]['received_weth'] += amount1Out
-
-            # Fetch token transfers for the block
-            token_transfers = get_token_transfers(wallet_address)
-
-            # Match transaction hashes to token addresses
-            for transfer in token_transfers.get('result', []):
-                tx_hash = transfer.get('transaction_hash')
-                token_address = transfer.get('address')
-
-                if tx_hash in transactions:
-                    transactions[tx_hash]['token_address'] = token_address
-                    total_trades += 1
+                            if is_buy:  # This is a buy
+                                transactions[transaction_hash]['spent_weth'] += amount1In
+                                transactions[transaction_hash]['received_tokens'] += amount0Out
+                                total_trades += 1
+                            else:  # This is a sell
+                                transactions[transaction_hash]['sold_tokens'] += amount0In
+                                transactions[transaction_hash]['received_weth'] += amount1Out
 
         # Calculate total profit and profitability
         total_realized_profit = 0.0
